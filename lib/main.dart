644711ad259de732e514c72bf7b4014f32ed7287
52sen <sein.jung@g2e.co.kr>
2024-07-07 11:15:03 +0900

    mofigy patch

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: '릴리즈 자동화'),
+      home: const MyHomePage(title: '릴리즈 자동dfadf화'),
     );
   }
 }
